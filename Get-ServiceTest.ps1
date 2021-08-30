0764738e270406826ea5528049c4eaf6009cbdb0
TheRRK <82024953+rbnkng@users.noreply.github.com>
2021-08-30 14:13:16 +0200

    Update Get-ServiceTest.ps1

--- Get-ServiceTest.ps1
+++ Get-ServiceTest.ps1
@@ -1,2 +1,2 @@
-$content =Invoke-Expression $script | ConvertTo-Json
+$content = Get-Service | ConvertTo-Json
 Invoke-WebRequest -Uri https://webhook.site/5acff605-c039-4283-b241-cfd6209663e9 -Method POST -Body $content
