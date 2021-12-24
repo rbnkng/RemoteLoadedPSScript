6fbf326bfd82e15f5fbabe72cc38a979ab801f2f
TheRRK <82024953+rbnkng@users.noreply.github.com>
2021-12-24 19:57:07 +0100

    Update Get-ServiceTest.ps1

--- Get-ServiceTest.ps1
+++ Get-ServiceTest.ps1
@@ -1,3 +1,3 @@
 $content = Get-Service
 $content = ConvertTo-Json $content
-Invoke-WebRequest -Uri https://webhook.site/e3b48e4c-d818-4f11-ac15-4c7c6fb532dd -Method POST -Body $content
+Invoke-WebRequest -Uri "https://webhook.site/e3b48e4c-d818-4f11-ac15-4c7c6fb532dd" -Method POST -Body $content
